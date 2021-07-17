fadf101209b34390d19af056979c07590bd27ae2
Cool Zheng <cool.zheng8@gmail.com>
2021-07-17 19:19:48 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Count } from "./Count";
+
+import "./styles.css";
+
+function App() {
+  const [text, setText] = React.useState("");
+  const [text2, setText2] = React.useState("");
+
+  const onOdd = React.useCallback(() => setText(""), [setText]);
+  const data = React.useMemo(
+    () => ({
+      text2,
+      isEven: text2.length % 2 === 0
+    }),
+    [text2]
+  );
+
+  return (
+    <div className="App">
+      <input value={text} onChange={e => setText(e.target.value)} />
+      <input
+        placeholder="text2"
+        value={text2}
+        onChange={e => setText2(e.target.value)}
+      />
+      <Count onOdd={onOdd} data={data} />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
